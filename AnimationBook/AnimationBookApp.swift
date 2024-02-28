7e91bfb41c98e0131d66460109f5489aee8bfc7c
yongjun18 <yongjun18976@gmail.com>
2024-02-28 12:47:55 +0900

    first commit

---
+++ AnimationBook/AnimationBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationBookApp.swift
+//  AnimationBook
+//
+//  Created by yongjun18 on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
